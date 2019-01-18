542f39a19638fe540df9009ff478090f00848aec
brendanmcewen <mceweb1@mcmaster.ca>
2019-01-18 11:27:39 -0500

    Submitting my assignment

---
+++ Homework_01.18.19.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+anoles <- read_csv("winchell_evol_phenshifts.csv")
+str(anoles)
+summary(anoles)        # Certain values here are being reported as characters (e.g. column 'context'; whether the
+                       #    lizard was caught in a natural or urbanized area)
+names(anoles)
+anoles <- (anoles                      
+           %>% mutate(ID=as.factor(ID),        
+                      Site=as.factor(Site),    
+                      context=as.factor(context),  
+                      perch=as.factor(perch),    
+                      perch.diam.cm=as.numeric(perch.diam.cm),
+                      flags=as.factor(flags))) 
+
+print(anoles
+      %>% group_by(Site,context)
+      %>% summarize(count=n())) # Want to see if they have roughly equal natural versus urban representation at each study site
+
+print(ggplot(anoles, aes(x=bodytemp.C))
+             +geom_histogram()
+)                                       # Reveals that we have a few anoles that are markedly colder than the others
+
+print(ggplot(anoles, aes(x=svl.mm))
+      +geom_histogram(binwidth=0.5)
+)                                       # Most lizards between 60-70mm Snout-Vent Length, handful of extreme outliers
+
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # QMEE
 Class Repository
 
-Homework from Wk 1, Jan 11 2019
+Homework from Wk 1 added on Jan 11 2019
+
+Homework from Wk 2: added on Jan 18. Summary(anoles) revealed that many of the variables had been stored as characters instead of factors, so I changed them all to factors. I then inspected the distribution of body temperature at time of capture, and the distribution of Snout-Vent Length in these anoles. 
+
+What I would like to do with my data is construct a model that allows me to predict the body temperature of a given lizard using data from its surroundings (i.e. whether it's in a natural or urban environment, what the ambient temperature is, what the humidity is, how high it's perched, etc.)
 
