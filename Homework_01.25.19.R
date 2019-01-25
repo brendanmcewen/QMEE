70f2f1f9bc3af6671812a1a571129461cf749285
brendanmcewen <mceweb1@mcmaster.ca>
2019-01-25 11:45:11 -0500

    Submitting my homework for Week 3: Visualization

---
+++ Homework_01.25.19.R
@@ -0,0 +1,110 @@
+library(tidyverse)
+library(ggplot2)
+
+anoles <- read_csv("winchell_evol_phenshifts.csv")
+
+anoles <- anoles %>% mutate(bodytemp.F = (32 + 1.8*bodytemp.C))   ## Converting to Fahrenheit to help my brain
+
+anoles <- (anoles
+           %>% mutate_at(vars(Site,context,perch,flags),funs(factor)) ## Turning my character read-ins into factors
+           %>% mutate(perch.diam.cm=parse_number(perch.diam.cm))
+)
+
+with(anoles, table(perch, context))  ## Want to see the total table of perch observations/categories, to see what I can collapse into tree/metal/concrete
+
+levels(anoles$perch) <- list(tree=c("tree", "woody branch", "behind a sign on a tree", "bush"),             ## Collapsing perches into three main levels, discarding the rest
+                             concrete=c("concrete", "concrete pos", "concrete wall", "conctreete wall"),
+                             metal=c("metal", "metal fence", "metal fence post", "metal gate", "metal pipe", 
+                                     "metal pole", "inside metal fence post"),
+                             other="NA"
+)        ## A more tidyverse-oriented way to do this would have been fct_collapse(tree="yadda", "yadda", "yadda") and repeat for concrete and metal 
+
+anoles$perch[is.na(anoles$perch)] <- "other"     ## Need to substitute "other" for all the NAs that are in there from when I collapsed my perch levels
+
+
+anoles %>% mutate(body.condition = (weight.g/svl.mm)*100)   ## This didn't create a new column in the dataframe like it did when I converted bodytemp.F from bodytemp.C
+                                                            ## So I did it a non-tidyverse way:
+anoles$body.condition <- (anoles$weight.g/anoles$svl.mm)*100  ## This more or less did what I wanted it to. 
+## Should I still be using residual svl/mass to calculate this type of variable?
+
+## Now on to the actual figure-making:
+
+## Now that we have a body condition metric, I want to see whether lizards differ in their body condition based upon their natural/urban environment
+
+(condition_context_box <- ggplot(anoles, aes(context,body.condition, fill=context))+
+  geom_boxplot()+
+  scale_fill_manual(values=c("forestgreen","gray"))+           ## Want intuitive colors for my boxplot fills
+  labs(x="Environmental Context", y="Body Condition", 
+       title="Anole Body Condition in Urban vs. Natural Environments",
+       fill="Context")+
+  theme(panel.border = element_rect(fill=NA, color="black"),
+          legend.title=element_text(size=11),                  ## Removing as much non-data ink as possible, while
+          legend.position=c(0.85,0.8),                         ##    keeping the outline of the figure
+          legend.text=element_text(size=11),
+          panel.grid.major = element_blank(),
+          axis.title.x = element_text(size=12),
+          axis.text.x = element_text(size=11),
+          axis.title.y = element_text(size=12),
+          axis.text.y = element_text(size=11),
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill="white"))
+) ## Nope, no real difference. There seems to be a bit more variation in urban environments but it's not enough to be interesting.
+
+## Let's see the relationship between physical body condition and thermal state, split by urban vs natural context:
+(condition_temp_fig <- ggplot(anoles, aes(x=body.condition, y=bodytemp.F))+
+  geom_point()+
+  geom_smooth(method=lm)+
+  facet_grid(cols=vars(context))
+)
+## These relationships are uninspiring, but a slight negative relationship could potentially be explained by the fact that it's harder to heat larger objects up at a fixed energy input
+
+## Let's see if higher mass of a lizard is associated with a greater perch diameter:
+tree_anoles <- filter(anoles, perch == "tree") ## subsetting to only include lizards found on trees
+
+(perch_mass_fig <- ggplot(tree_anoles, aes(weight.g, perch.diam.cm))+
+  geom_point()+
+  geom_smooth(method=lm)+
+  facet_grid(cols=vars(context))
+)      ## Once again, really nothing interesting going on, but we see a heavier concentration of small-diameter capture points in natural settings. How about I box-plot that?
+
+(perch_diam_box <- ggplot(tree_anoles, aes(context, perch.diam.cm, fill=context))+
+  geom_boxplot()
+)
+
+
+## I wonder if larger lizards are found closer or farther from the ground compared to small lizards? Larger lizards might perceive themselves safer in a given 
+## environment than a smaller lizard would, thus I predict larger lizards to spend more time close to the ground (and thus be captured closer to the ground)
+## This also yielded an uninspiring relationship. I'll just find ways to make my boring data more visually interesting
+(perch_height_mass_fig <- ggplot(anoles, aes(weight.g, perch.height.cm, shape=Site, color=Site))+
+  labs(x="Mass (g)", y="Perch Height (cm)",
+       title="Anole Perch Height by Mass",
+       shape="Site")+
+  geom_point(aes(shape=factor(Site)))+
+  facet_grid(cols=vars(context))+
+  theme(panel.border = element_rect(fill=NA, color="black"),
+          legend.title=element_text(size=11),                  ## Removing as much non-data ink as possible, while
+          legend.position=c(0.8,0.8),                          ##    keeping the outline of the figure
+          legend.text=element_text(size=11),
+          panel.grid.major = element_blank(),
+          axis.title.x = element_text(size=12),
+          axis.text.x = element_text(size=11),
+          axis.title.y = element_text(size=12),
+          axis.text.y = element_text(size=11),
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill="white"))
+)
+## This is still a mess. How about looking at body temperature by substrate type?
+
+
+
+(substrate_temp_fig <- ggplot(anoles, aes(perch,bodytemp.F,fill=perch))+
+  geom_boxplot()+                                                                                                                
+  labs(x="Substrate", y="Body Temperature (F)")+
+  scale_x_discrete(labels=c("Tree","Concrete","Metal","Other"))+
+  scale_fill_manual(values=c("forestgreen","grey95","lightsteelblue3","thistle")) 
+)        ## It seems that lizards caught on concrete substrates tend to have a slightly higher internal temperature. This is odd as I would have predicted metal-dwelling lizards
+         ## To be hotter, unless those hotter surfaces become intolerable and the lizards avoid them. 
+
+
+## I keep getting error messages that I'm removing rows containing non-finite values. What could be the source of this?
+
--- README.md
+++ README.md
@@ -5,5 +5,17 @@ Homework from Wk 1 added on Jan 11 2019
 
 Homework from Wk 2: added on Jan 18. Summary(anoles) revealed that many of the variables had been stored as characters instead of factors, so I changed them all to factors. I then inspected the distribution of body temperature at time of capture, and the distribution of Snout-Vent Length in these anoles. 
 
-What I would like to do with my data is construct a model that allows me to predict the body temperature of a given lizard using data from its surroundings (i.e. whether it's in a natural or urban environment, what the ambient temperature is, what the humidity is, how high it's perched, etc.)
+What I would like to do with my data is construct a model that allows me to predict the body temperature of a given lizard using data from its surroundings (i.e. whether it's in a natural or urban environment, what the ambient temperature is, what the humidity is, how high it's perched, etc.) 
+
+Homework from Week 3:
+My goal for this assignment is to:
+  A. Visualize the relationship between individual body condition and thermal state (bodytemp.F) in a scatter plot
+      Define body condition as each indiv's mass divided by their SVL 
+      Use facets to display this between urban and natural environments
+  B. Visualize body temperatures by substrate type (e.g. do lizards caught from a tree have diff. temp than if caught from a concrete substrate?)
+      Will bin 'perch' elements into three categories and re-name as substrate: Tree, Concrete (collapse all concrete elements), and metal (collapse all metal elements). Entries of perch elements that do not fill these categories will be removed from the data. 
+      Will construct box plots to visualize this. 
+      
+About these data: This dataset is not my own; I found it on Dryad. I just searched 'Anolis' and sorted by recent uploads to find some modern lizard data to see what types of data other scientists working with Anolis are collecting. I noticed that they included lots of thermal data, which piqued my interest in the potential effects that thermal state may have on lizard behavior. I would like to try my hand at creating a model predicting body temperature as a function of many environmental parameters. I could then potentially take this to the field and see if there's any relationship between observed Flight Initiation Distance and the body temperature predicted by whatever model I'm able to come up with. One could also examine the effect that individual differences in thermal state has on aggressive interactions; I would predict an individual at a higher thermal state to signal more aggressively, as they would be able to exert themselves more physically and would potentially incur less cost during a contest than they would at a comparatively lower thermal states. Although, if you have enough time to set up and observe an aggressive interaction, you would probably have enough time to capture and measure the body temperature of whatever lizard you were observing instead of relying on a model to approximate that for you 
+
 
